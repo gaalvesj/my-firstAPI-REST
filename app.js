65b8f891ffe8f4e717434c65277f577bfd9247e9
Gabriel <gabrielalves3654@gmail.com>
2022-11-03 15:54:29 -0300

    Mandando Requisicao e Resposta com hoppscoth

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // import express from 'express';
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.use(feedRoutes)
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+
+app.use('/feed', feedRoutes)
 
 app.listen(8080, console.log('rodando!! na porta 8080'));
