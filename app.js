8613b51560d2de738856e4b8e3d58bbc741b7e86
Gabriel <gabrielalves3654@gmail.com>
2022-11-03 15:06:22 -0300

    Route setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// first commit
+// import express from 'express';
+const express = require('express');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+app.use(feedRoutes)
+
+app.listen(8080, console.log('rodando!! na porta 8080'));
